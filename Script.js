91a147c0046b84bd0d0aaf92cb4494ae4103f98a
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-09-28 04:15:09 +0200

    changed to query selector

--- Script.js
+++ Script.js
@@ -21,39 +21,40 @@ const check = () => {
   document.getElementById("password").value = input;
   document.getElementById("count").innerText = "Length : " + input.length;
 
+
   if (input.length >= 8) {
     document.getElementById("check0").style.color = "green";
-    document.getElementById("eight").innerText = "Password contains 8 characters";
+    document.querySelector("#check0 span").innerText = "Password contains 8 characters";
   } else {
     document.getElementById("check0").style.color = "red";
-    document.getElementById("eight").innerText = "Password does not contain 8 characters";
+    document.querySelector("#check0 span").innerText = "Password does not contain 8 characters";
   }
 
   if (input.match(/\d/)) {
     document.getElementById("check1").style.color = "green";
-    document.getElementById("numbCheck").innerText = "Password contains a number";
+    document.querySelector("#check1 span").innerText = "Password contains a number";
   } else {
     document.getElementById("check1").style.color = "red";
-    document.getElementById("numbCheck").innerText = "Password does not contain a number";
+    document.querySelector("#check1 span").innerText = "Password does not contain a number";
   }
 
   if (input.match(/[A-Z]/)) {
     document.getElementById("check2").style.color = "green";
-    document.getElementById("capital").innerText = "Password does contain a capital letter";
+    document.querySelector("#check2 span").innerText = "Password does contain a capital letter";
 
   } else {
     document.getElementById("check2").style.color = "red";
-    document.getElementById("capital").innerText = "Password does not contain a capital letter";
+    document.querySelector("#check2 span").innerText = "Password does not contain a capital letter";
 
   }
 
   if (input.match(/[^A-Za-z0-9-''/i]/)) {
-    document.getElementById("check3").style.color = "green";
-    document.getElementById("special").innerText = "Password does contain a special character";
+    document.querySelector("#check3").style.color = "green";
+    document.querySelector("#check3 span").innerText = "Password does contain a special character";
 
   } else {
     document.getElementById("check3").style.color = "red";
-    document.getElementById("special").innerText = "Password does not contain a special character";
+    document.querySelector("#check3 span").innerText = "Password does not contain a special character";
 
   }
 }
