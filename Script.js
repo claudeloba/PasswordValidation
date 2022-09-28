e613e44b77c3d8265064709ba1193f685ecb9637
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-09-28 03:31:09 +0200

    change text

--- Script.js
+++ Script.js
@@ -23,25 +23,37 @@ const check = () => {
 
   if (input.length >= 8) {
     document.getElementById("check0").style.color = "green";
+    document.getElementById("eight").innerText = "Password contains 8 characters";
   } else {
     document.getElementById("check0").style.color = "red";
+    document.getElementById("eight").innerText = "Password does not contain 8 characters";
   }
 
   if (input.match(/\d/)) {
     document.getElementById("check1").style.color = "green";
+    document.getElementById("numbCheck").innerText = "Password contains a number";
   } else {
     document.getElementById("check1").style.color = "red";
+    document.getElementById("numbCheck").innerText = "Password does not contain a number";
   }
 
   if (input.match(/[A-Z]/)) {
     document.getElementById("check2").style.color = "green";
+    document.getElementById("capital").innerText = "Password does contain a capital letter";
+
   } else {
     document.getElementById("check2").style.color = "red";
+    document.getElementById("capital").innerText = "Password does not contain a capital letter";
+
   }
 
   if (input.match(/[^A-Za-z0-9-''/i]/)) {
     document.getElementById("check3").style.color = "green";
+    document.getElementById("special").innerText = "Password does contain a special character";
+
   } else {
     document.getElementById("check3").style.color = "red";
+    document.getElementById("special").innerText = "Password does not contain a special character";
+
   }
 }
